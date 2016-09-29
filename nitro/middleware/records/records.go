fc1c4e8876f3d476ed056ddd5d631ddbe2547075
Alexander Ewetumo <trinoxf@gmail.com>
2016-09-29 18:43:39 +0100

    updating core

--- nitro/middleware/records/records.go
+++ nitro/middleware/records/records.go
@@ -1,1 +1,6 @@
 package records
+
+// RecordMW is a struct which implements the netd.Middleware interface. It provides a new message
+// processor for a RECORD message format that allows clients to send efficient record/model transactions
+// over the wire.
+type RecordWM struct{}
