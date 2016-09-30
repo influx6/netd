6cb80cca79d6f8846ef0dafa9931a51a429aca08
Alexander Ewetumo <trinoxf@gmail.com>
2016-09-30 11:06:13 +0100

    setting core

--- nitro/middleware/records/records.go
+++ nitro/middleware/records/records.go
@@ -1,8 +1,20 @@
 package records
 
+import "github.com/influx6/netd"
+
+type Cache interface {
+}
+
 // RecordMW is a struct which implements the netd.Middleware interface. It provides a new message
 // processor for a RECORD message format that allows clients to send efficient record/model transactions
 // over the wire.
-type RecordWM struct{}
+type RecordWM struct {
+	cache Cache
+}
 
-func (RecordWM) HandleEvents()
+// HandleEvents provides a method which implements the netd.Middleware interface
+// and handles the subscription if needed for connect and disconnect events.
+func (RecordWM) HandleEvents(context interface{}, cx netd.ConnectionEvents) error {
+
+	return nil
+}
