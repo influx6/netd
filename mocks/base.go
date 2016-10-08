0e99f848a264a27b399ef71f9b61964438812be1
Alexander Ewetumo <trinoxf@gmail.com>
2016-10-08 19:42:10 +0100

    adding basic mocks

--- mocks/base.go
+++ mocks/base.go
@@ -3,36 +3,54 @@ package mocks
 import (
 	"bytes"
 
-	"github.com/influx6/netd"
+	. "github.com/influx6/netd"
 )
 
 //==============================================================================
 
-// ConnectionsMocks defines a mock for the netd.Connections interface.
+// NewConnection returns a mock Connection instance which provides a mock implementation
+// for use with netd.Providers.
+func NewConnection(client, server *BaseInfo, router Router, receiver Receiver, stat StatProvider, parser MessageParser) *Connection {
+	return &Connection{
+		Base:         client,
+		Server:       server,
+		Router:       router,
+		Stat:         stat,
+		Parser:       parser,
+		Connections:  &ConnectionsMock{},
+		Subscriber:   &SubscriberMock{Receiver: receiver},
+		Messager:     &MessagerMock{},
+		DeferRequest: &DeferRequestMock{},
+	}
+}
+
+//==============================================================================
+
+// ConnectionsMocks defines a mock for the Connections interface.
 type ConnectionsMock struct {
-	ClientList      []netd.BaseInfo
-	ClusterList     []netd.BaseInfo
+	ClientList      []BaseInfo
+	ClusterList     []BaseInfo
 	ClusterMessages bytes.Buffer
 	ClientMessages  bytes.Buffer
 }
 
-// Clients implements the netd.Connections.Clients method.
-func (c *ConnectionsMock) Clients(context interface{}) netd.SearchableInfo {
-	return netd.SearchableInfo(c.ClientList)
+// Clients implements the Connections.Clients method.
+func (c *ConnectionsMock) Clients(context interface{}) SearchableInfo {
+	return SearchableInfo(c.ClientList)
 }
 
-// Clusters implements the netd.Connections.Clusters method.
-func (c *ConnectionsMock) Clusters(context interface{}) netd.SearchableInfo {
-	return netd.SearchableInfo(c.ClientList)
+// Clusters implements the Connections.Clusters method.
+func (c *ConnectionsMock) Clusters(context interface{}) SearchableInfo {
+	return SearchableInfo(c.ClientList)
 }
 
-// SendToClusters implements the netd.Connections.SendToClusters method.
+// SendToClusters implements the Connections.SendToClusters method.
 func (c *ConnectionsMock) SendToClusters(context interface{}, id string, msg []byte, flush bool) error {
 	c.ClusterMessages.Write(msg)
 	return nil
 }
 
-// SendToClient implements the netd.Connections.SendToClient method.
+// SendToClient implements the Connections.SendToClient method.
 func (c *ConnectionsMock) SendToClients(context interface{}, id string, msg []byte, flush bool) error {
 	c.ClientMessages.Write(msg)
 	return nil
@@ -40,25 +58,28 @@ func (c *ConnectionsMock) SendToClients(context interface{}, id string, msg []by
 
 //==============================================================================
 
-// SubscriberMock implements the netd.Subscriber interface.
+// Receiver defines the function type for Submessage reception.
+type Receiver func(context interface{}, sm *SubMessage) error
+
+// SubscriberMock implements the Subscriber interface.
 type SubscriberMock struct {
-	Receiver func(context interface{}, sm *netd.SubMessage) error
+	Receiver Receiver
 }
 
 // Fire implements the Fire call for the subscriber.
-func (s *SubscriberMock) Fire(context interface{}, sm *netd.SubMessage) error {
+func (s *SubscriberMock) Fire(context interface{}, sm *SubMessage) error {
 	return s.Receiver(context, sm)
 }
 
 //==============================================================================
 
-// MessagerMock is a mock implementation for the netd.Messager interface.
+// MessagerMock is a mock implementation for the Messager interface.
 type MessagerMock struct {
 	EMessages bytes.Buffer
 	NMessages bytes.Buffer
 }
 
-// Send implements the Send method for the netd.Messager.
+// Send implements the Send method for the Messager.
 func (s *MessagerMock) Send(context interface{}, flush bool, msgs ...[]byte) error {
 	for _, msg := range msgs {
 		if err := s.SendMessage(context, msg, flush); err != nil {
@@ -69,7 +90,7 @@ func (s *MessagerMock) Send(context interface{}, flush bool, msgs ...[]byte) err
 	return nil
 }
 
-// SendError implements the SendError method for the netd.Messager.
+// SendError implements the SendError method for the Messager.
 func (s *MessagerMock) SendError(context interface{}, flush bool, msgs ...error) error {
 	for _, msg := range msgs {
 		s.EMessages.Write([]byte(msg.Error()))
@@ -78,7 +99,7 @@ func (s *MessagerMock) SendError(context interface{}, flush bool, msgs ...error)
 	return nil
 }
 
-// SendMessage implements the SendMessage method for the netd.Messager.
+// SendMessage implements the SendMessage method for the Messager.
 func (s *MessagerMock) SendMessage(context interface{}, msg []byte, flush bool) error {
 	s.NMessages.Write(msg)
 	return nil
@@ -86,20 +107,20 @@ func (s *MessagerMock) SendMessage(context interface{}, msg []byte, flush bool) 
 
 //==============================================================================
 
-// DeferRequestMock implements a mock for the netd.DeferRequest interface.
+// DeferRequestMock implements a mock for the DeferRequest interface.
 type DeferRequestMock struct {
-	DMessages []netd.Message
+	DMessages []Message
 }
 
-// Defer implements the netd.DeferRequest.Defer method.
-func (d *DeferRequestMock) Defer(context interface{}, msg ...netd.Message) error {
+// Defer implements the DeferRequest.Defer method.
+func (d *DeferRequestMock) Defer(context interface{}, msg ...Message) error {
 	d.DMessages = append(d.DMessages, msg...)
 	return nil
 }
 
 //==============================================================================
 
-// ClusterMock implements the netd.ClusterConnect interface.
+// ClusterMock implements the ClusterConnect interface.
 type ClusterMock struct{}
 
 // NewCluster implements a function for creating new clusters for communicating
--- mocks/logging.go
+++ mocks/logging.go
@@ -6,54 +6,54 @@ import (
 	"io"
 )
 
-// Logger defines a base logger which logs all into a provided writer.
-type Logger struct {
+// MockLogger defines a base logger which logs all into a provided writer.
+type MockLogger struct {
 	in io.Writer
 }
 
-// NewLogger returns a new Logger instance.
-func NewLogger(in io.Writer) *Logger {
-	return &Logger{in: in}
+// NewMockLogger returns a new Logger instance.
+func NewMockLogger(in io.Writer) *MockLogger {
+	return &MockLogger{in: in}
 }
 
 // Log provides a normal-level log which allows different levels to be log with.
-func (l *Logger) Log(context interface{}, fn string, message string, data ...interface{}) {
+func (l *MockLogger) Log(context interface{}, fn string, message string, data ...interface{}) {
 	fmt.Fprintf(l.in, "Context[%s] :  Function[%s] :  %s\n", context, fn, fmt.Sprintf(message, data...))
 }
 
 // Error provides a error-level log function.
-func (l *Logger) Error(context interface{}, fn string, err error, message string, data ...interface{}) {
+func (l *MockLogger) Error(context interface{}, fn string, err error, message string, data ...interface{}) {
 	fmt.Fprintf(l.in, "Context[%s] :  Function[%s] :  %s : Error[%s]\n", context, fn, fmt.Sprintf(message, data...), err)
 }
 
-// Tracer defines a data trace provider which uses a internal buffer to collect all trace then
+// MockTracer defines a data trace provider which uses a internal buffer to collect all trace then
 // clears when ended.
-type Tracer struct {
+type MockTracer struct {
 	b  bytes.Buffer
 	in io.Writer
 }
 
-// NewTracer returns a new instance of a tracer.
-func NewTracer(in io.Writer) *Tracer {
-	var t Tracer
+// NewTracerMock returns a new instance of a tracer.
+func NewMockTracer(in io.Writer) *MockTracer {
+	var t MockTracer
 	t.in = in
 	return &t
 }
 
 // Begin begins the trace call for the giving message.
-func (t *Tracer) Begin(context interface{}, msg []byte) {
+func (t *MockTracer) Begin(context interface{}, msg []byte) {
 	t.b.Write([]byte(fmt.Sprintf("--TRACE [%+q] Begin------------------------------\n", msg)))
 }
 
 // Trace logs the trace call for the giving message.
-func (t *Tracer) Trace(context interface{}, msg []byte) {
+func (t *MockTracer) Trace(context interface{}, msg []byte) {
 	t.b.Write(msg)
 	t.b.Write([]byte("\n"))
 }
 
 // End ends the trace call for the giving message and clears the internal buffer, writing the trace
 // into the internal writer.
-func (t *Tracer) End(context interface{}, msg []byte) {
+func (t *MockTracer) End(context interface{}, msg []byte) {
 	t.b.Write([]byte(fmt.Sprintf("--TRACE [%+q] Ended------------------------------\n", msg)))
 	t.in.Write(t.b.Bytes())
 	t.b.Reset()
